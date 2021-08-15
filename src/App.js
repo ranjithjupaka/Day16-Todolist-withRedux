52c3d4f317c531032d292dfab191e6efd72dbf2d
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-08-15 17:40:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './Actions/TodoActions';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState();
+  const dispatch = useDispatch();
+  const Todo = useSelector((state) => state.Todo);
+  const { todos } = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(AddTodoAction(todo))
+  }
+  
+  const handleRemove = (t) => {
+    dispatch(RemoveTodoAction(t));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h2> Todo List with redux</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder='Enter a todo'
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: 'none',
+              fontSize: 20,
+            }}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            type='submit'
+            style={{
+              borderRadius: 50,
+              height: 50,
+              width: 50,
+              marginLeft: 20,
+            }}
+          >
+            GO
+          </button>
+        </form>
+        <ul className='allTodos'>
+          {todos &&
+            todos.map((t) => (
+              <li key={t.id} className='singleTodo'>
+                <span className='todoText'>{t.todo}</span>
+                <button
+                  type='button'
+                  style={{
+                    borderRadius: 25,
+                    padding: 10,
+                    border: '1px solid white',
+                    color: 'white',
+                    backgroundColor: 'orangered',
+                  }}
+                  onClick={() => handleRemove(t)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+        </ul>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware,combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk"
+import {TodoReducer} from './Reducers/TodoReducer'
+
+
+const reducer =combineReducers({
+    Todo : TodoReducer,
+})
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
